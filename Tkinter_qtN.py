cdc45cac77faa629f3b5733962a756cc8bb42e7e
storm9789 <jreinsmith@olivetcollege.edu>
2016-01-18 15:15:33 -0500

    Changed most code

--- Tkinter_qtN.py
+++ Tkinter_qtN.py
@@ -1,41 +1,26 @@
-from tkinter import *
+import tkinter
 
-
-class Example(Frame):
-
-    def __init__(self, parent):
-        Frame.__init__(self, parent, background = "white")
-
-        self.parent = parent
-
-        self.initUI()
-
-
-    def initUI(self):
-
-        self.parent.title("Simple")
-        self.pack(fill = BOTH, expand = 1)
 
 def the_box():
-    tkMessageBox.showinfo("ERROR!!!!!!", "Wazzzaaa!")
-# this is the current Error msg.
-# ===== RESTART: C:\Users\storm\Documents\GitHub\Tkinter_qtNotepad\tab.py =====
-# Traceback (most recent call last):
-#   File "C:\Users\storm\Documents\GitHub\Tkinter_qtNotepad\tab.py", line 33, in <module>
-#     main()
-#   File "C:\Users\storm\Documents\GitHub\Tkinter_qtNotepad\tab.py", line 27, in main
-#     B = tkinter.Button(top, text ="hello", command = the_box)
-# NameError: name 'tkinter' is not defined
-# >>> 
+    main()
+
+
 def main():
 
-    root = Tk()
+    root = tkinter.Tk()
     root.geometry("250x150+300+300")
-    app = Example(root)
-    B = tkinter.Button(top, text ="hello", command = the_box)
-    B.pack()
+    app = tkinter.Frame(root)
+    app.pack()
+    t = tkinter.Text(app)
+    t.pack(fill = "both", expand = 1)
+    B = tkinter.Button(text = "New Tab", command = the_box)
+    B.pack(side = "top")
     root.mainloop()
-    
+
+# Opens tkinter with a textbox that resizes?
+# button does not show above the textbox
+# the_box needs to be reworked in such a way to make new
+# tabs have the same contents as the main
 
 if __name__ == '__main__':
     main()
