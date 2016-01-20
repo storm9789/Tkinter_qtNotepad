377f68c5985b1ccc800df3a820d5d9cf52c6f736
storm9789 <jreinsmith@olivetcollege.edu>
2016-01-20 10:32:48 -0500

    Widgets still dont know there parents!!!

--- Tkinter_qtN.py
+++ Tkinter_qtN.py
@@ -2,13 +2,21 @@ import tkinter
 
 
 def the_box():
-    main()
+    new_window = tkinter.Toplevel()
+    new_window.geometry("800x600+300+300")
+    app = tkinter.Frame(new_window)
+    app.pack()
+    t = tkinter.Text(app)
+    t.pack(fill = "both", expand = 1)
+    B = tkinter.Button(text = "New Tab", command = the_box)
+    B.pack(side = "top")
+    new_window.mainloop()
 
 
 def main():
 
     root = tkinter.Tk()
-    root.geometry("250x150+300+300")
+    root.geometry("800x600+300+300")
     app = tkinter.Frame(root)
     app.pack()
     t = tkinter.Text(app)
@@ -16,7 +24,10 @@ def main():
     B = tkinter.Button(text = "New Tab", command = the_box)
     B.pack(side = "top")
     root.mainloop()
-
+# The main problem seems to be defining the parent
+# tried opening the new window as top level was unsuccessful
+# This was most likely because it does not put it atop the widget hierarchy
+# -------------------------------------------
 # Opens tkinter with a textbox that resizes?
 # button does not show above the textbox
 # the_box needs to be reworked in such a way to make new
