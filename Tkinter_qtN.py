af8bdd614e3be2fe96134fa6bc649d922b2a6186
storm9789 <jreinsmith@olivetcollege.edu>
2016-01-25 10:39:32 -0500

    Breaks, No errors.

--- Tkinter_qtN.py
+++ Tkinter_qtN.py
@@ -1,37 +1,115 @@
-import tkinter
+# File: notebook.py
+#    http://docs.python.org/py3k/library/tkinter.ttk.html?highlight=ttk#notebook
+ 
+from tkinter import *
+from tkinter import ttk
+#from demopanels import MsgPanel, SeeDismissPanel
+ 
+class NotebookDemo(ttk.Frame):
+     
+    def __init__(self, isapp=True, name='notebookdemo'):
+        ttk.Frame.__init__(self, name=name)
+        self.pack(expand=Y, fill=BOTH)
+        self.master.title('Notebook Demo')
+        self.isapp = isapp
+        self._create_widgets()
+         
+    def _create_widgets(self):
+        if self.isapp:           
+            #SeeDismissPanel(self)
+            pass
+         
+        self._create_demo_panel()
+         
+    def _create_demo_panel(self):
+        demoPanel = Frame(self, name='demo')
+        demoPanel.pack(side=TOP, fill=BOTH, expand=Y)
+                 
+        # create the notebook
+        nb = ttk.Notebook(demoPanel, name='notebook')
+ 
+        # extend bindings to top level window allowing
+        #   CTRL+TAB - cycles thru tabs
+        #   SHIFT+CTRL+TAB - previous tab
+        #   ALT+K - select tab using mnemonic (K = underlined letter)
+        nb.enable_traversal()
+         
+        nb.pack(fill=BOTH, expand=Y, padx=2, pady=3)
+        self._create_descrip_tab(nb)
+        self._create_disabled_tab(nb)
+        self._create_text_tab(nb)
+                        
+    def _create_descrip_tab(self, nb):
+        # frame to hold contentx
+        frame = ttk.Frame(nb, name='descrip')
+         
+        # widgets to be displayed on 'Description' tab
+        msg = ["Ttk is the new Tk themed widget set. One of the widgets ",
+               "it includes is the notebook widget, which provides a set ",
+               "of tabs that allow the selection of a group of panels, ",
+               "each with distinct content. They are a feature of many ",
+               "modern user interfaces. Not only can the tabs be selected ",
+               "with the mouse, but they can also be switched between ",
+               "using Ctrl+Tab when the notebook page heading itself is ",
+               "selected. Note that the second tab is disabled, and cannot "
+               "be selected."]
+         
+        lbl = ttk.Label(frame, wraplength='4i', justify=LEFT, anchor=N,
+                        text=''.join(msg))
+        neatVar = StringVar()
+        btn = ttk.Button(frame, text='New Tab', underline=0,
+                         command=lambda v=neatVar: self._say_neat(v))
+        neat = ttk.Label(frame, textvariable=neatVar, name='neat')
+         
+        # position and set resize behaviour
+        lbl.grid(row=0, column=0, columnspan=2, sticky='new', pady=5)
+        btn.grid(row=1, column=0, pady=(2,4))
+        neat.grid(row=1, column=1,  pady=(2,4))
+        frame.rowconfigure(1, weight=1)
+        frame.columnconfigure((0,1), weight=1, uniform=1)
+         
+        # bind for button short-cut key
+        # (must be bound to toplevel window)
+        self.winfo_toplevel().bind('<Alt-n>', lambda e, v=neatVar: self._say_neat(v))
+         
+        # add to notebook (underline = index for short-cut character)
+        nb.add(frame, text='Description', underline=0, padding=2)
+         
+    def _say_neat(self, v):
+        v.set('Creating Tab')
+        # -----creates new tab---*currently breaks program(NO ERROR MESSAGE)
+        demoPanel = Frame(self, name='demo')
+        demoPanel.pack(side=TOP, fill=BOTH, expand=Y)
+        nb = ttk.Notebook(demoPanel, name='notebook')
+        frame = ttk.Frame(nb, name='descrip')
+        nb.add(frame, text='Text_Edt', underline=0, padding=2)
+        #---------------------
+        self.update()
+        self.after(500, v.set(''))
+ 
+    # =============================================================================
+    def _create_disabled_tab(self, nb):
+        # Populate the second pane. Note that the content doesn't really matter
+        frame = ttk.Frame(nb)
+        nb.add(frame, text='Disabled', state='disabled')
+ 
+    # =============================================================================
+    def _create_text_tab(self, nb):
+        # populate the third frame with a text widget
+        frame = ttk.Frame(nb)
+         
+        txt = Text(frame, wrap=WORD, width=40, height=10)
+        vscroll = ttk.Scrollbar(frame, orient=VERTICAL, command=txt.yview)
+        txt['yscroll'] = vscroll.set
+        vscroll.pack(side=RIGHT, fill=Y)
+        txt.pack(fill=BOTH, expand=Y)
+         
+        # add to notebook (underline = index for short-cut character)
+        nb.add(frame, text='Text Editor', underline=0)
 
-
-def the_box():
-    new_window = tkinter.Toplevel()
-    new_window.geometry("800x600+300+300")
-    app = tkinter.Frame(new_window)
-    app.pack()
-    t = tkinter.Text(app)
-    t.pack(fill = "both", expand = 1)
-    B = tkinter.Button(text = "New Tab", command = the_box)
-    B.pack(side = "top")
-    new_window.mainloop()
-
-
-def main():
-
-    root = tkinter.Tk()
-    root.geometry("800x600+300+300")
-    app = tkinter.Frame(root)
-    app.pack()
-    t = tkinter.Text(app)
-    t.pack(fill = "both", expand = 1)
-    B = tkinter.Button(text = "New Tab", command = the_box)
-    B.pack(side = "top")
-    root.mainloop()
-# The main problem seems to be defining the parent
-# tried opening the new window as top level was unsuccessful
-# This was most likely because it does not put it atop the widget hierarchy
-# -------------------------------------------
-# Opens tkinter with a textbox that resizes?
-# button does not show above the textbox
-# the_box needs to be reworked in such a way to make new
-# tabs have the same contents as the main
 
 if __name__ == '__main__':
-    main()
+    NotebookDemo().mainloop()
+
+
+
